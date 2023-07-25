cfbfab9bd16816ce33fb543b6c2a5891e5287522
Ali Hakimi <ali.hakimi.njf@gmail.com>
2023-07-25 10:17:49 +0330

    create servise to get data

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { DataService } from './services/data.service';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -12,8 +14,10 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    HttpClientModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers:[DataService]
 })
 export class HomePageModule {}
